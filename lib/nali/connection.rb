7e0a8823bf311b01dd9a2fe09cc38236fc2e5ab6
4urbanoff <4urbanoff@gmail.com>
2014-11-09 08:01:24 +0300

    remove filters, add multi sync

--- lib/nali/connection.rb
+++ lib/nali/connection.rb
@@ -3,33 +3,10 @@ module EventMachine
     class Connection
          
       def reset
-        @filters = {} 
         @storage = {} 
         @watches = {} 
       end
-      
-      def filters( model_name )
-        @filters ||= {} 
-        @filters[ model_name ] ||= []
-      end
-        
-      def filters_add( model_name, new_filter )
-        unless( new_filter.keys.include?( :id ) and new_filter.keys.length == 1 )
-          exist = false
-          filters( model_name ).each { |filter| exist = true if filter.sort.to_s == new_filter.sort.to_s }
-          filters( model_name ) << new_filter unless exist
-        end
-      end
-        
-      def filter?( model )
-        filters( model.class.name.to_sym ).each do |filter|
-          result = true
-          filter.each { |key, value| result = false if model[ key ] != value }
-          return result if result
-        end
-        false
-      end
-      
+
       def []( name )
         @storage ||= {} 
         @storage[ name ] or nil
@@ -68,12 +45,14 @@ module EventMachine
         send hash.to_json
       end
         
-      def sync( model )
-        params, relations = model.get_sync_params( self )
-        if not params.empty? and ( watch_time( model ) < model.updated_at.to_f or model.destroyed? )
-          if model.destroyed? then unwatch( model ) else watch_time_up model end
-          relations.each { |relation| sync relation }
-          send_json action: :sync, params: params 
+      def sync( *models )
+        models.flatten.compact.each do |model|
+          params, relations = model.get_sync_params( self )
+          if not params.empty? and ( watch_time( model ) < model.updated_at.to_f or model.destroyed? )
+            if model.destroyed? then unwatch( model ) else watch_time_up model end
+            relations.each { |relation| sync relation }
+            send_json action: :sync, params: params
+          end
         end
       end
         
