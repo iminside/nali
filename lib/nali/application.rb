b6ef17b75cdd1fd9a2f30e53b7d93283ae9e42d3
4urbanoff <4urbanoff@gmail.com>
2014-11-14 13:53:04 +0300

    restructure app

--- lib/nali/application.rb
+++ lib/nali/application.rb
@@ -25,7 +25,7 @@ module Nali
       client.append_path File.join( Nali.path, 'client/javascripts' )
 
       %w( app/client/templates app/client/stylesheets app/client/javascripts lib/client/stylesheets
-          lib/client/javascripts public/client vendor/client/stylesheets vendor/client/javascripts
+          lib/client/javascripts vendor/client/stylesheets vendor/client/javascripts
       ).each { |path| client.append_path File.join( root, path ) }
 
       Sprockets::Helpers.configure do |config|
--- lib/nali/tasks.rb
+++ lib/nali/tasks.rb
@@ -35,10 +35,7 @@ module Nali
         
         desc 'Remove compiled client files'
         task :clean do
-          compiled_path = File.join @settings.public_folder, 'client'
-          Dir[ compiled_path + '/*' ]
-          .select { |file| file =~ /(application|manifest).*?\.(js|css|json)/ }
-          .each { |file| File.delete file }
+          FileUtils.rm_rf File.join( @settings.public_folder, 'client' )
           index = File.join( @settings.public_folder, 'index.html' )
           File.delete( index ) if File.exists?( index )
           puts 'Compiled client files removed'
